5fb468350208ebca23b0fef2ae89ec5a7ae207de
cqnsinger <82493653+cqnsinger@users.noreply.github.com>
2021-10-29 13:33:37 +0700

    Update BaiTapLon.cpp

--- BaiTapLon.cpp
+++ BaiTapLon.cpp
@@ -1,59 +1,97 @@
 #include<iostream>
-#include<string.h>
-#include<conio.h>
 using namespace std;
-class DOB{
-	private:
-		int day, month, year;
-	public:
-		friend istream& operator >> (istream &i, DOB &s){
-			cout<<"Nhap Ngay Thang Nam Sinh: "<<endl;
-			cout<<"\tNhap ngay sinh: ";
-			i>>day;
-			cout<<"\tNhap thang: ";
-			i>>month;
-			cout<<"\tNhap nam sinh: ";
-			i>>year;
-		}
-	};
-class info{
+class score{
+	protected:
+		float s_toan, s_tin, s_eng;
+};
+class infoST:public score{
 	private:
 		float GPA;
 		string ID, Class, Major;
+	public:
+		void nhapI(){
+			cout<<"Nhap ma sinh vien: ";
+			fflush(stdin);
+			getline(cin, ID);
+			cout<<"Nhap chuyen nganh: ";
+			fflush(stdin);
+			getline(cin, Major);
+			cout<<"Nhap lop: ";
+			fflush(stdin);
+			getline(cin, Class);
+			cout<<"Nhap diem Toan: ";
+			cin>>s_toan;
+			cout<<"Nhap diem Tin: ";
+			cin>>s_tin;
+			cout<<"Nhap diem Tieng Anh: ";
+			cin>>s_eng;
+			GPA=(s_toan+s_tin+s_eng)/3;
+			}
+		void xuatI(){
+		
+			}
 	};
-class SinhVien:private info{
+class SinhVien:public infoST{
 	private: 
-		string Name, Gender;
-		DOB ns;
+		string Name, Gender, DOB;
 	public:
 		void Nhap();
 		void Xuat();
+		void NhapDS(SinhVien s, int &n);
+		void XuatDS(SinhVien s, int n);
 	};
-typedef SinhVien Item;
 struct Node{
-	Item data;
+	SinhVien data;
 	Node *next;
 };
 void SinhVien::Nhap(){
-	 
+	cout<<"Nhap ho ten: ";
+	fflush(stdin);
+	getline(cin, Name);
+	cout<<"Nhap gioi tinh: ";
+	fflush(stdin);
+	getline(cin, Gender);
+	cout<<"Nhap ngay sinh: ";
+	fflush(stdin);
+	getline(cin, DOB);
+	nhapI();
+	}
 struct SList{
 	Node *head;
 	Node *tail;
 	long size;
 	SList();
-	Node* CreateNode(Item v);
+	Node* CreateNode(SinhVien v);
+	void addST(SinhVien v);
 	};
 SList::SList(){
 	head = NULL;
 	tail = NULL;
 	size = 0;
 }
-
-Node* SList::CreateNode(Item v)
-{
+void SinhVien::NhapDS(SinhVien s, int &n){
+	cout<<"Nhap so sinh vien: ";
+	cin>>n;
+	for(int i=0; i<n; i++){
+		Nhap();
+		}
+	}
+Node* SList::CreateNode(SinhVien v){
 	Node* p = new Node;
-	p->data = v;	
+	p->data = v;
 	p->next = NULL;	
 	return p; 
 }
-
+void SList::addST(SinhVien v){
+	Node* p = CreateNode(v);
+	p->next = head;
+	head = p;
+	if (size == 0)
+		tail = p;
+	size++;
+	}
+int main(){
+	SinhVien s;
+	int n;
+	s.NhapDS(s, n);
+	}
